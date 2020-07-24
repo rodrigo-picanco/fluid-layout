9c61be35210025629ac10b265aec7be1ec5ee36c
rodrigo picanco <rodrigopicancotavares@gmail.com.br>
2020-07-23 20:34:12 -0400

    feat: box  new dpcs

--- src/box/box.ts
+++ src/box/box.ts
@@ -5,11 +5,15 @@ import { prop, theme } from 'styled-tools'
 // Types
 import { BoxProps } from './interface'
 
+/**
+ * The Box is the building block of your apps. It can receive four props. It's padding, it's border and it's color and it's backgroundColor.  The latter of default's to the styled-components theme in colors.background, or the #fff color.
+ */
 export const Box = styled.div<BoxProps>`
   background-color: ${prop(
     'backgroundColor',
     theme('colors.background', '#fff')
   )};
+  color: ${prop('color', theme('colors.color', '#000'))};
   padding: ${prop('padding', '1rem')};
   border: ${prop('border')};
 `
--- src/box/interface.ts
+++ src/box/interface.ts
@@ -1,7 +1,17 @@
+// Packages
 import * as CSS from 'csstype'
+/** 
+  Defines the Box Component props
 
+ @interface BoxProps
+ @property {CSS.PaddingProperty} [padding]  The css padding of the box
+ @property {CSS.BorderProperty} [border]  The css border of the box
+ @property {CSS.PaddingProperty} [backgroundColor]  The css background-color of the box
+ @property {CSS.ColorProperty} [color]  The css color of the box
+ */
 export interface BoxProps {
-  padding?: CSS.PaddingProperty<1>
-  border?: CSS.BorderProperty<1>
+  padding?: CSS.PaddingProperty<string>
+  border?: CSS.BorderProperty<string>
   backgroundColor?: CSS.BackgroundColorProperty
+  color?: CSS.ColorProperty
 }
