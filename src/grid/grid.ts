f9a2b4fec3856bcf1419bf44eb1486fe089c5541
rodrigo picanco <rodrigopicancotavares@gmail.com.br>
2020-07-25 14:20:59 -0400

    feat: new grid docs

--- src/grid/grid.ts
+++ src/grid/grid.ts
@@ -1,8 +1,14 @@
+// Packages
 import styled from 'styled-components'
 import { prop } from 'styled-tools'
 
+// Types
 import { GridProps } from './interface'
 
+
+/**
+ * Grid is a responsive css grid with fixed columns. You define the space between the items with the "gutter" prop and the minimal collumn width with the "min" prop. The Grid does the rest. 
+ */
 export const Grid = styled.div<GridProps>`
   display: grid;
   grid-gap: ${prop('gutter')};
--- src/grid/interface.ts
+++ src/grid/interface.ts
@@ -1,5 +1,13 @@
+// Packages
 import * as CSS from 'csstype'
 
+/**
+ * Defines the props of the Grid component
+ *
+ * @interface GridProps
+ * @property {CSS.GridGapProperty} gutter the css grid-gutter of the grid 
+ * @property {CSS.MaxWidthProperty} min the minimal width of a collumn in the grid 
+ */
 export interface GridProps {
   gutter: CSS.GridGapProperty<string>
   min: CSS.MaxWidthProperty<string>
